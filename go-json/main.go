20ff4a5380c50b85c8c96b126674d2545065148c
Andrew Salko <andrewsalko@gmail.com>
2023-07-08 18:49:03 +0300

    Go json sample

---
+++ go-json/main.go
@@ -0,0 +1,21 @@
+﻿package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Go json sample app")
+
+	doc1 := DocumentInfo{Name: "Andrew", Id: 10, Description: []string{"Hello", "World!"}}
+
+	doc1.Description = []string{"apple", "pie", "Hello"}
+
+	jsonBytes, _ := json.Marshal(doc1)
+	jsonText := string(jsonBytes)
+
+	fmt.Println(jsonText)
+
+	fmt.Println("Finished")
+} //main
